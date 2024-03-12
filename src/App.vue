332548f8846e7bf5f85bff4d0b82185073231eb1
Fedorov Denis <ssboshen@yandex.ru>
2024-03-12 14:38:29 +0300

    use Attributes from parent component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
+</script>
+
+<template>
+  <TheHeader />
+
+  <main class="flex flex-grow flex-col">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus suscipit nobis dicta sed
+    libero consequuntur distinctio, sequi molestias hic velit?
+  </main>
+
+  <TheNav />
+</template>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<template>
+  <li class="flex-1">
+    <a class="flex flex-col text-sx items-center capitalize p-2" v-bind="$attrs">
+      <slot></slot>
+    </a>
+  </li>
+</template>
